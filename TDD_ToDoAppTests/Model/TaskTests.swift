d068e1ed08981371301da1bbb90d79568be203eb
Maxim Mitin <maximus405@gmail.com>
2021-12-31 20:08:45 +0300

    coding via TDD
    Create and new struct Task

---
+++ TDD_ToDoAppTests/Model/TaskTests.swift
@@ -0,0 +1,42 @@
+//
+//  TaskTests.swift
+//  TDD_ToDoAppTests
+//
+//  Created by Maxim Mitin on 31.12.21.
+//
+
+import XCTest
+@testable import TDD_ToDoApp
+
+class TaskTests: XCTestCase {
+    
+    func testInitTaskWithTitle() {
+        let task = Task(title: "Foo")
+        
+        XCTAssertNotNil(task)
+    }
+    
+    func testInitTaskWithTitleAndDesc() {
+        let task = Task(title: "Foo", description: "Bar")
+        
+        XCTAssertNotNil(task)
+    }
+    
+    func testWhenGivenTitleSetsTitle() {
+        let task = Task(title: "Foo")
+        
+        XCTAssertEqual(task.title, "Foo")
+    }
+    
+    func testWhenGivenDescSetsDesc() {
+        let task = Task(title: "Foo", description: "Bar")
+        
+        XCTAssertEqual(task.description, "Bar")
+    }
+    
+    func testTasksInitsWithDate() {
+        let task = Task(title: "Foo", description: "Bar")
+        
+        XCTAssertNotNil(task.date)
+    }
+}
