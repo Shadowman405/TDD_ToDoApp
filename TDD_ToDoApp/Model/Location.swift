f45d229b98576ed25b1d252a5e6467754435f05e
Maxim Mitin <maximus405@gmail.com>
2022-01-01 20:54:42 +0300

    Creating Location model and cover it with test

---
+++ TDD_ToDoApp/Model/Location.swift
@@ -0,0 +1,26 @@
+//
+//  Location.swift
+//  TDD_ToDoApp
+//
+//  Created by Maxim Mitin on 1.01.22.
+//
+
+import Foundation
+import CoreLocation
+
+struct Location {
+    let name: String
+    let coordinate: CLLocationCoordinate2D?
+    
+    init(name: String, coordinate: CLLocationCoordinate2D? = nil) {
+        self.name = name
+        self.coordinate = coordinate
+    }
+}
+
+extension Location: Equatable {
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        guard lhs.coordinate?.latitude == rhs.coordinate?.latitude && lhs.coordinate?.longitude == rhs.coordinate?.longitude && lhs.name == rhs.name else {return false}
+        return true
+    }
+}
