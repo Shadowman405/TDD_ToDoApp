793b1fb4eeef108aaebcac4466deb99be3be8cdd
Maxim Mitin <maximus405@gmail.com>
2022-01-01 21:36:21 +0300

    more functions in TaskManager and more test coverage

--- TDD_ToDoApp/Manager/TaskManager.swift
+++ TDD_ToDoApp/Manager/TaskManager.swift
@@ -9,17 +9,31 @@ import Foundation
 
 
 class TaskManager {
-    var tasksCount = 0
-    var doneTasksCount = 0
+    var tasksCount: Int {
+        return tasks.count
+    }
+    var doneTasksCount: Int {
+        return doneTasks.count
+    }
     
     private var tasks: [Task] = []
+    private var doneTasks: [Task] = []
     
     func add(task: Task) {
-        tasksCount += 1
         tasks.append(task)
     }
     
     func task(at index: Int) -> Task {
         return tasks[index]
+    }
+    
+    func checkTask(at index: Int) {
+        let task = tasks.remove(at: index)
+        doneTasks.append(task)
+    }
+    
+    func doneTask(at index: Int) -> Task {
+        
+        return doneTasks[index]
     }
 }
