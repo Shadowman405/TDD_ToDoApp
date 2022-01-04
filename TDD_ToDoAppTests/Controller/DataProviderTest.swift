48b212e8c7b58a4ff20c54151e382d1328852bb3
Maxim Mitin <maximus405@gmail.com>
2022-01-04 23:23:32 +0300

    rework DataProvider (sections logic) test for DoneTask section

--- TDD_ToDoAppTests/Controller/DataProviderTest.swift
+++ TDD_ToDoAppTests/Controller/DataProviderTest.swift
@@ -89,7 +89,19 @@ class DataProviderTest: XCTestCase {
         let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as! MockTaskCell
         
         XCTAssertEqual(cell.task, task)
+    }
+    
+    func testCellForRowInSectionOneCallsConfigure() {
+        tableView.register(MockTaskCell.self, forCellReuseIdentifier: String(describing: TaskCell.self))
         
+        let task = Task(title: "Foo")
+        sut.taskManager?.add(task: task)
+        sut.taskManager?.checkTask(at: 0)
+        tableView.reloadData()
+        
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 1)) as! MockTaskCell
+        
+        XCTAssertEqual(cell.task, task)
     }
 
 }
