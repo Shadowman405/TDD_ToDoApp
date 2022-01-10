fc8f5c6866df48f78b7f2b9a33c699fc280d67d1
Maxim Mitin <maximus405@gmail.com>
2022-01-10 21:11:28 +0300

    add outlets for textfields and buttons, all cover with test. Save button logic and tests

--- TDD_ToDoApp/Controller/NewTaskViewController.swift
+++ TDD_ToDoApp/Controller/NewTaskViewController.swift
@@ -6,7 +6,39 @@
 //
 
 import UIKit
+import CoreLocation
 
 class NewTaskViewController: UIViewController {
+    
+    var taskManager: TaskManager!
+    var geocoder = CLGeocoder()
+    
     @IBOutlet var titleTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var addresstextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    var dateFormater: DateFormatter{
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        return df
+    }
+    
+    func save() {
+        let titleString = titleTextField.text
+        let locationString = locationTextField.text
+        let date = dateFormater.date(from: dateTextField.text!)
+        let descriptionString = descriptionTextField.text
+        let addressString = addresstextField.text
+        geocoder.geocodeAddressString(addressString!) {[unowned self] (placemarks,error) in
+            let placemark = placemarks?.first
+            let coordinate = placemark?.location?.coordinate
+            let location = Location(name: locationString!, coordinate: coordinate)
+            let task = Task(title: titleString!, description: descriptionString, location: location, date: date)
+            self.taskManager.add(task: task)
+        }
+    }
 }
--- TDD_ToDoAppTests/Controller/NewTaskViewControllerTests.swift
+++ TDD_ToDoAppTests/Controller/NewTaskViewControllerTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 @testable import TDD_ToDoApp
+import MapKit
 
 class NewTaskViewControllerTests: XCTestCase {
     
@@ -25,4 +26,51 @@ class NewTaskViewControllerTests: XCTestCase {
     func testHasTitletextField() {
         XCTAssertTrue(sut.titleTextField.isDescendant(of: sut.view))
     }
+    
+    func testHasLocationtextField() {
+        XCTAssertTrue(sut.locationTextField.isDescendant(of: sut.view))
+    }
+    
+    func testHasDatetextField() {
+        XCTAssertTrue(sut.dateTextField.isDescendant(of: sut.view))
+    }
+    
+    func testHasAddresstextField() {
+        XCTAssertTrue(sut.addresstextField.isDescendant(of: sut.view))
+    }
+    
+    func testHasDescriptionTextField() {
+        XCTAssertTrue(sut.descriptionTextField.isDescendant(of: sut.view))
+    }
+    
+    func testHasCancelButton() {
+        XCTAssertTrue(sut.cancelButton.isDescendant(of: sut.view))
+    }
+    
+    func testHasSaveButton() {
+        XCTAssertTrue(sut.saveButton.isDescendant(of: sut.view))
+    }
+    
+    func testSaveUsesGeocoderToConvertCoordinateFromAddress() {
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        let date = df.date(from: "01.01.21")
+        
+        sut.titleTextField.text = "Foo"
+        sut.locationTextField.text = "Bar"
+        sut.dateTextField.text = "01.01.21"
+        sut.addresstextField.text = "Minsk"
+        sut.descriptionTextField.text = "Baz"
+        
+        sut.taskManager = TaskManager()
+        sut.save()
+        
+        let task = sut.taskManager.task(at: 0)
+        let coordinate = CLLocationCoordinate2D(latitude: 53.896196, longitude: 27.5503093)
+        let location = Location(name: "Bar", coordinate: coordinate)
+        let generatedTask = Task(title: "Foo", description: "Baz", location: location, date: date)
+        
+        XCTAssertEqual(task, generatedTask)
+    }
+    
 }
